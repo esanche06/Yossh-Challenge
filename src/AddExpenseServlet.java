8a4b44bfc7f13cb16e4780b4a4e5e88adc343feb
esanche06 <ethands@uci.edu>
2018-05-02 15:00:26 -0700

    Added index page and template Java backend.
    I also added a context.xml so I can run MySQL queries on my local computer, I may have to tweak that later when I deploy to the "production" server. Also added a bootstrap template navbar to use on both pages. Finally, I added a template Java servlet file, next thing I'm going to do is make sure that I can actually communicate with the backend through Javascript.

---
+++ src/AddExpenseServlet.java
@@ -0,0 +1,86 @@
+import com.google.gson.JsonObject;
+
+import javax.annotation.Resource;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.SQLException;
+
+//
+@WebServlet(name = "AddExpenseServlet", urlPatterns = "/api/add-expense")
+public class AddExpenseServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Resource(name = "jdbc/expenses")
+    private DataSource dataSource;
+    
+    private void sendError(HttpServletResponse response) {
+        try {
+			JsonObject responseJsonObject = new JsonObject();
+			responseJsonObject.addProperty("status", "fail");
+			responseJsonObject.addProperty("message", "Could not add expense.");
+			response.getWriter().write(responseJsonObject.toString());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+    
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        PrintWriter out = response.getWriter();
+        
+        Connection dbCon = null;
+        Statement statement = null;
+        
+        try {
+	        dbCon = dataSource.getConnection();
+            statement = dbCon.createStatement();
+            
+            String query = "";
+    		statement.executeUpdate(query);
+    		
+		
+	        JsonObject responseJsonObject = new JsonObject();
+	        responseJsonObject.addProperty("status", "success");
+	        responseJsonObject.addProperty("message", "Added expense to the DB.");
+	
+	        response.getWriter().write(responseJsonObject.toString());
+        }catch(SQLException se){
+        	sendError(response);
+            se.printStackTrace();
+        }catch(Exception e){
+        	sendError(response);
+    		e.printStackTrace();
+    		
+    		out.println("<body>");
+    		out.println("<p>");
+    		out.println("Exception in doGet: " + e.getMessage());
+    		out.println("</p>");
+    		out.print("</body>");
+        }finally {
+        	//finally block used to close resources
+            try{
+            	if(statement!=null)
+            		dbCon.close();
+            }catch(SQLException se){
+            }// do nothing
+            try{
+               if(dbCon!=null)
+                  dbCon.close();
+            }catch(SQLException se){
+               se.printStackTrace();
+            }//end finally try
+        }
+    }
+}
